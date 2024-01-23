68ecfa7bcfee589b985a1218970a9c7c771b7edb
TroyMena <troymena@gmail.com>
2024-01-23 17:49:35 -0500

    Added code to have the Cloudinairy API work with our application.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const cloudinary = require('cloudinary').v2;
+const bodyParser = require('body-parser');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -9,7 +11,7 @@ const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const sess = {
   secret: 'Super secret secret',
@@ -32,15 +34,48 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// body parser configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(routes);
+
+// cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
 // For Cloudinairy:
-app.use((request, response) => {
-  response.json({ message: 'Hey! This is your server response!' }); 
+app.get("/", (request, response) => {
+  response.json({ message: "Hey! This is your server response!" });
+});
+
+// image upload API
+app.post("/image-upload", (request, response) => {
+  // collected image from a user
+  const data = {
+    image: request.body.image,
+  }
+
+  // upload image here
+  cloudinary.uploader.upload(data.image)
+  .then((result) => {
+    response.status(200).send({
+      message: "success",
+      result,
+    });
+  }).catch((error) => {
+    response.status(500).send({
+      message: "failure",
+      error,
+    });
+  });
 });
 
 module.exports = app;
