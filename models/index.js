855f163aac8052c76f88c01b8318ba7f7c38362a
bsaglimb <bsaglimb>
2024-01-26 01:36:08 -0500

    removed Gallery.js and Painting.js files and updated the index.js accorindly. removed more unnecessary commented out code

--- models/index.js
+++ models/index.js
@@ -1,16 +1,6 @@
 const User = require('./User');
-const Gallery = require('./Gallery');
-const Painting = require('./Painting');
 const Survey = require('./Survey');
 const Profile = require('./Profile'); 
-
-Gallery.hasMany(Painting, {
-  foreignKey: 'gallery_id',
-});
-
-Painting.belongsTo(Gallery, {
-  foreignKey: 'gallery_id',
-});
 
 User.hasOne(Survey, {
   foreignKey: 'user_id',
@@ -30,4 +20,5 @@ Profile.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
-module.exports = { User, Gallery, Painting, Survey, Profile };
+module.exports = { User, Survey, Profile };
+
