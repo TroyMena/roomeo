f09cc6f587d3e317e61917deebe3e9c250029815
bsaglimb <bsaglimb>
2024-01-26 01:24:00 -0500

    removed the galleryData.js from seeds, and updated code in index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,10 @@
 const sequelize = require('../config/connection');
 const seedSurvey = require('./surveyData');
-const seedGallery = require('./galleryData');
-
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   await seedSurvey();
-
-  await seedGallery();
 
   process.exit(0);
 };
