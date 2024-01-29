cad02b14d8e37529462a880bd72c058647f6ea9e
TroyMena <troymena@gmail.com>
2024-01-29 02:14:09 -0500

    Removed two lines to see if I can fix an error in Heroku (a possible port error?)

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const normalizePort = val => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+//const port = normalizePort(process.env.PORT || '3000');
+//app.set('port', port);
 
 const errorHandler = error => {
   if (error.syscall !== 'listen') {
