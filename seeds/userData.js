7f734d91dea29fe9f330932df1f606768024fad1
TroyMena <troymena@gmail.com>
2024-01-29 16:33:15 -0500

    Added additional data for members of Roomeo.

--- seeds/userData.js
+++ seeds/userData.js
@@ -22,6 +22,46 @@ const userdata = [
     email: "gypsy@msn.com",
     password: "password12345"
   }, 
+  {
+    username: 'Jonathan Jones',
+    email: "gfdgdfgfd554ffg@gmail.com",
+    password: "Fsfdsfds443!"
+  }, 
+  {
+    username: 'Emily Anderson',
+    email: "gfgr56dfg@gmail.com",
+    password: "Fsfdsfds443!"
+  }, 
+  {
+    username: 'Jim Smith',
+    email: "THEJimSmith@gmail.com",
+    password: "Fsfdsfds443!"
+  }, 
+  {
+    username: 'Maria Gonzalez',
+    email: "gdsfzgzds@gmail.com",
+    password: "Fsfdsfds443!"
+  }, 
+  {
+    username: 'Kevin Johnson',
+    email: "gfhfgd@gmail.com",
+    password: "Fsfdsfds443!"
+  }, 
+  {
+    username: 'Gertrude Walters',
+    email: "fdasfgretre@gmail.com",
+    password: "Fsfdsfds443!"
+  }, 
+  {
+    username: 'Justin Drew Bieber',
+    email: "JustinOfficial@JustinBieber.com",
+    password: "Fsfdsfds443!"
+  }, 
+  {
+    username: 'Archibald Cyrus Kennedy VI',
+    email: "goodjob@gmail.com",
+    password: "Fsfdsfds443!"
+  }, 
 ];
 
 const seedUsers = () => User.bulkCreate(userdata);
